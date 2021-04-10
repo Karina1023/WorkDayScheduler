bc245a82337692bf2e83b9038246528bb3178501
Karina Ayala <karinaayala2021@u.northwestern.edu>
2021-04-10 15:38:33 -0500

    updating script.js file and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# WorkDayScheduler
+WORK DAY SCHEDULER
+
+I created an application that runs in the browser and features dynamically updated HTML, and CSS powered by JQuery.
+
+The work day scheduler allows users to save events for each hour of the day. Each timeblock is color coordinated to show whether it is past present or future. When the save button is clicked, the information for that day is saved in local storage.
+
+Screenshot:
+/Users/macbook/Desktop/WorkDayScheduler/Screen Shot 2021-04-10 at 3.25.20 PM.png
+
+https://github.com/Karina1023/WorkDayScheduler.git
+ https://karina1023.github.io/WorkDayScheduler/
--- script.js
+++ script.js
@@ -1,14 +1,57 @@
 $(document).ready(function() {
     $('#currentDay').text(moment().format("MMMM, Do YYYY, h:mm:ss a"));
     
-//use this to compare to timeblocks
-    var currentHour = moment().hours()
-    console.log(currentHour)
-    $(".time-block").each(function() {
-        //this is targeting the id on the timeblock and making it a number
-    var blockId = parseInt($(this).attr("id"))
+//assign savebtn click listener for user input and time stamp
+    $(".saveBtn").on("click", function () {
+        //get nearby values
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+// set items in local storage
+       localStorage.setItem(time, text);
+
     })
+    //load any saved data from localstorage 
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+
+
+    function hourTracker() {
+        //get current number of hours
+        var currentHour = moment().hour();
+//loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(blockHour, currentHour)
+   //if else statements
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
     
+        })
+
+    }
+    hourTracker();
+
 })
 
 //GIVEN I am using a daily planner to create a schedule
